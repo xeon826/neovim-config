f1f585546e00560752cad7dc099dfb9b7fcc9005
Joshua <xeon826@gmail.com>
2024-06-13 12:37:16 -0500

    bufferline setup

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,13 +2,13 @@ return {{
   'akinsho/bufferline.nvim',
   requires = 'kyazdani42/nvim-web-devicons', -- if you're using icons
   config = function()
-    -- local bufferline = require('bufferline')
-    -- bufferline.setup({
-    --   options = {
-    --     separator_styling = 'padded_slope'
-    --   }
-    --   }
-    -- )
+    local bufferline = require('bufferline')
+    bufferline.setup({
+      options = {
+        separator_styling = 'padded_slope'
+      }
+      }
+    )
   end,
   event = 'VimEnter' -- Ensures it starts after Neovim finishes loading
 }}
