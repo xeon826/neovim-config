b5747dcda28297c18be140edc600c3e88cc966de
Joshua <xeon826@gmail.com>
2025-02-16 18:32:05 -0600

    command to sync to remote device

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -7,18 +7,37 @@ return {
 				"<leader>ds",
 				mode = { "n" },
 				function()
-					local salt = vim.fn.getenv("SALT")
-					if not salt then
-						vim.notify("SALT environment variable not set", vim.log.levels.ERROR)
+					local pw = vim.fn.getenv("MAC_PW")
+					local user = vim.fn.getenv("MAC_USER")
+					local host = vim.fn.getenv("MAC_HOST")
+					if not pw then
+						vim.notify("MAC_PW environment variable not set", vim.log.levels.ERROR)
 						return
 					end
+					if not user or not host then
+						vim.notify("MAC_USERNAME or MAC_HOST environment variables not set", vim.log.levels.ERROR)
+						return
+					end
+					local current_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
 					local cmd = string.format(
-						'rsync -avz --delete -e \'sshpass -p %s ssh -o StrictHostKeyChecking=no\' "%s/" "dan@192.168.100.2:/Users/dan/sync/"',
-						salt,
-						vim.fn.getcwd()
+						'rsync -avz --delete -R -e \'sshpass -p %s ssh -v -o StrictHostKeyChecking=no\' "%s/" "%s@%s:/Users/%s/sync/%s"',
+						pw,
+						'.',
+						user,
+						host,
+						user,
+						current_dir
 					)
-					vim.fn.system(cmd)
-					vim.notify("Sync completed", vim.log.levels.INFO)
+					
+					-- Execute command and capture output
+					local output = vim.fn.system(cmd)
+					local exit_code = vim.v.shell_error
+					
+					if exit_code ~= 0 then
+						vim.notify("Sync failed:\n" .. output, vim.log.levels.ERROR)
+					else
+						vim.notify("Sync completed successfully", vim.log.levels.INFO)
+					end
 				end,
 				desc = "Sync project to remote",
 			},
