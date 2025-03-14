74fc7bf2b1d725f93e609dc6fc6e4d815bd68d12
Joshua <xeon826@gmail.com>
2025-03-14 09:45:42 -0500

    remove close unpinned functionality

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -42,20 +42,6 @@ M.my_git_commits = function(opts)
 	builtin.git_commits(opts)
 end
 
-function M.close_unpinned_buffers()
-	local bufferline = require("bufferline")
-	local commands = require("bufferline.commands")
-	local current = vim.api.nvim_get_current_buf()
-	vim.schedule(function()
-		for _, e in ipairs(bufferline.get_elements().elements) do
-			local is_current = e.id == current
-			local is_pinned = bufferline.groups._is_pinned(e)
-			if not is_current and not is_pinned then
-				commands.unpin_and_close(e.id)
-			end
-		end
-	end)
-end
 
 function M.get_ipy_kernel_name()
 	local venv = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX")
--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,18 +2,32 @@ local utils = require("config/utils")
 return {
 	{
 		"akinsho/bufferline.nvim",
-		requires = "kyazdani42/nvim-web-devicons", -- if you're using icons
+		requires = "kyazdani42/nvim-web-devicons",
 		keys = {
 			{ "<C-S-L>", "<cmd>BufferLineMoveNext<CR>", { noremap = true, silent = true, desc = "Move buffer right" } },
 			{ "<C-S-H>", "<cmd>BufferLineMovePrev<CR>", { noremap = true, silent = true, desc = "Move buffer left" } },
 			{ "<S-h>", "<cmd>BufferLineCyclePrev<CR>", { noremap = true, silent = true, desc = "Previous buffer" } },
 			{ "<S-l>", "<cmd>BufferLineCycleNext<CR>", { noremap = true, silent = true, desc = "Next buffer" } },
 			{
-				"<leader>bcu",
-				utils.close_unpinned_buffers,
-				{ noremap = true, silent = true, desc = "Close unpinned buffers" },
+				"<leader>bd",
+				function()
+					-- Get current buffer number
+					local current_buf = vim.api.nvim_get_current_buf()
+
+					-- Check if nvim-tree is loaded and visible
+					local nvim_tree_loaded = package.loaded["nvim-tree"]
+					if nvim_tree_loaded then
+						local api = require("nvim-tree.api")
+						if api.tree.is_visible() then
+							api.tree.close()
+						end
+					end
+
+					-- Close the current buffer using bdelete
+					vim.cmd("bdelete " .. current_buf)
+				end,
+				{ noremap = true, silent = true, desc = "Delete current buffer" },
 			},
-			{ "<leader>bd", "<cmd>NvimTreeClose<cr><cmd>bd<CR>", { noremap = true, silent = true, desc = "Delete current buffer" } },
 			{
 				"<leader>bp",
 				"<cmd>BufferLineTogglePin<CR>",
@@ -33,6 +47,6 @@ return {
 				},
 			})
 		end,
-		event = "VimEnter", -- Ensures it starts after Neovim finishes loading
+		event = "VimEnter",
 	},
 }
