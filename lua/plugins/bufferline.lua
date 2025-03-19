45f757323e226c0b06062615217fdb101105093c
Joshua <xeon826@gmail.com>
2025-03-18 22:38:53 -0500

    bufferline hotkey fix

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,10 +4,10 @@ return {
 		"akinsho/bufferline.nvim",
 		requires = "kyazdani42/nvim-web-devicons",
 		keys = {
-			{ "<C-S-L>", "<cmd>BufferLineMoveNext<CR>", { noremap = true, silent = true, desc = "Move buffer right" } },
-			{ "<C-S-H>", "<cmd>BufferLineMovePrev<CR>", { noremap = true, silent = true, desc = "Move buffer left" } },
-			{ "<S-h>", "<cmd>BufferLineCyclePrev<CR>", { noremap = true, silent = true, desc = "Previous buffer" } },
-			{ "<S-l>", "<cmd>BufferLineCycleNext<CR>", { noremap = true, silent = true, desc = "Next buffer" } },
+			{ "<C-A-L>", "<cmd>BufferLineMoveNext<CR>", noremap = true, silent = true, desc = "Move buffer right" },
+			{ "<C-A-H>", "<cmd>BufferLineMovePrev<CR>", noremap = true, silent = true, desc = "Move buffer left" },
+			{ "<S-h>", "<cmd>BufferLineCyclePrev<CR>", noremap = true, silent = true, desc = "Previous buffer" },
+			{ "<S-l>", "<cmd>BufferLineCycleNext<CR>", noremap = true, silent = true, desc = "Next buffer" },
 			{
 				"<leader>bd",
 				function()
@@ -26,17 +26,51 @@ return {
 					-- Close the current buffer using bdelete
 					vim.cmd("bdelete " .. current_buf)
 				end,
-				{ noremap = true, silent = true, desc = "Delete current buffer" },
+				noremap = true,
+				silent = true,
+				desc = "Delete current buffer"
 			},
 			{
-				"<leader>bp",
+				"<leader>btp",
 				"<cmd>BufferLineTogglePin<CR>",
-				{ noremap = true, silent = true, desc = "Toggle pin for current buffer" },
+				noremap = true,
+				silent = true,
+				desc = "Toggle pin for current buffer"
 			},
 			{
 				"<leader>bco",
 				"<cmd>BufferLineCloseOthers<CR>",
-				{ noremap = true, silent = true, desc = "Close all aside from the current buffer" },
+				noremap = true,
+				silent = true,
+				desc = "Close all aside from the current buffer"
+			},
+			{
+				"<leader>bcr",
+				"<cmd>BufferLineCloseRight<CR>",
+				noremap = true,
+				silent = true,
+				desc = "Close tab to the right"
+			},
+			{
+				"<leader>bcl",
+				"<cmd>BufferLineCloseLeft<CR>",
+				noremap = true,
+				silent = true,
+				desc = "Close tab to the left"
+			},
+			{
+				"<leader>bp",
+				"<cmd>BufferLinePick<CR>",
+				noremap = true,
+				silent = true,
+				desc = "Buffer line pick"
+			},
+			{
+				"<leader>bcp",
+				"<cmd>BufferLinePickClose<CR>",
+				noremap = true,
+				silent = true,
+				desc = "Buffer line close pick"
 			},
 		},
 		config = function()
