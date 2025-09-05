44a3383d3cafa2d49de8ef0db0bbbbb03375c1c3
Joshua <xeon826@gmail.com>
2025-09-05 10:15:42 -0500

    avante switch to deepseek

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -16,18 +16,14 @@ return {
 		---@module 'avante'
 		---@type avante.Config
 		opts = {
-			-- add any opts here
-			-- for example
-			provider = "claude",
-			providers = {
-				claude = {
-					endpoint = "https://api.anthropic.com",
-					model = "claude-sonnet-4-20250514",
-					timeout = 30000, -- Timeout in milliseconds
-					extra_request_body = {
-						temperature = 0.75,
-						max_tokens = 20480,
-					},
+			provider = "deepseek",
+			vendors = {
+				deepseek = {
+					__inherited_from = "openai",
+					api_key_name = "DEEPSEEK_API_KEY",
+					endpoint = "https://api.deepseek.com",
+					model = "deepseek-coder",
+					max_tokens = 8192,
 				},
 			},
 		},
--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,6 +1,5 @@
 return {
 	"linux-cultist/venv-selector.nvim",
-  branch='regexp',
 	dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
 	config = function()
 		require("venv-selector").setup({
