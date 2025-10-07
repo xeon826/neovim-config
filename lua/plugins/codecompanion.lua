31503f893cd8e1a3142b2d5cb29085039a01054c
Joshua <xeon826@gmail.com>
2025-10-07 12:34:25 -0500

    mcp server

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -15,10 +15,10 @@ return { -- lazy.nvim
 				},
 				strategies = {
 					chat = {
-						adapter = {
-							name = "copilot",
-							model = "gpt-5-mini",
-						},
+						-- adapter = {
+						-- 	name = "copilot",
+						-- 	model = "gpt-5-mini",
+						-- },
 						roles = {
 							user = "dan",
 						},
@@ -72,30 +72,40 @@ return { -- lazy.nvim
 							},
 						},
 					},
-					inline = {
-						adapter = {
-							name = "copilot",
-							model = "gpt-4.1",
+					-- inline = {
+					-- 	adapter = {
+					-- 		name = "copilot",
+					-- 		model = "gpt-4.1",
+					-- 	},
+					-- },
+					tools = {
+						["mcp"] = {
+							callback = require("mcphub.extensions.codecompanion"),
+							description = "Call tools and resources from the MCP Servers",
+							opts = {
+								user_approval = true,
+							},
 						},
 					},
 				},
-				-- extensions = {
-				-- 	mcphub = {
-				-- 		callback = "mcphub.extensions.codecompanion",
-				-- 		opts = {
-				-- 			-- MCP Tools
-				-- 			make_tools = true, -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
-				-- 			show_server_tools_in_chat = true, -- Show individual tools in chat completion (when make_tools=true)
-				-- 			add_mcp_prefix_to_tool_names = false, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
-				-- 			show_result_in_chat = true, -- Show tool results directly in chat buffer
-				-- 			format_tool = nil, -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
-				-- 			-- MCP Resources
-				-- 			make_vars = true, -- Convert MCP resources to #variables for prompts
-				-- 			-- MCP Prompts
-				-- 			make_slash_commands = true, -- Add MCP prompts as /slash commands
-				-- 		},
-				-- 	},
-				-- },
+
+				extensions = {
+					mcphub = {
+						callback = "mcphub.extensions.codecompanion",
+						opts = {
+							-- MCP Tools
+							make_tools = true, -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
+							show_server_tools_in_chat = true, -- Show individual tools in chat completion (when make_tools=true)
+							add_mcp_prefix_to_tool_names = false, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
+							show_result_in_chat = true, -- Show tool results directly in chat buffer
+							format_tool = nil, -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
+							-- MCP Resources
+							make_vars = true, -- Convert MCP resources to #variables for prompts
+							-- MCP Prompts
+							make_slash_commands = true, -- Add MCP prompts as /slash commands
+						},
+					},
+				},
 			})
 		end,
 		keys = {
