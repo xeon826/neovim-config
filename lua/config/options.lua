d096d6dcc051598a81b5e33591011aaf18aeb1d3
Joshua <xeon826@gmail.com>
2025-11-17 09:47:14 -0600

    feat(ui): switch to oxocarbon colorscheme and enable transparent backgrounds for improved aesthetics

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -134,4 +134,8 @@ end
 
 -- Colorscheme
 -- By default, use rose-pine
-cmd.colorscheme("tokyonight-night")
+-- cmd.colorscheme("tokyonight-night")
+cmd.colorscheme("oxocarbon")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
